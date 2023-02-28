fa2e37acae2240b79e8db3118de7f6b023a315b8
La100 <stolamarcin@gmail.com>
2023-02-28 20:44:01 +0100

    elo

--- medusa-config.js
+++ medusa-config.js
@@ -24,10 +24,10 @@ try {
 }
 
 // CORS when consuming Medusa from admin
-const ADMIN_CORS = process.env.ADMIN_CORS || "http://localhost:7000,http://localhost:7001";
+const ADMIN_CORS = process.env.ADMIN_CORS || "https://admin.przykladowy.info";
 
 // CORS to avoid issues when consuming Medusa from a client
-const STORE_CORS = process.env.STORE_CORS || "http://localhost:8000";
+const STORE_CORS = process.env.STORE_CORS || "https://sklep.przykladowy.info";
 
 // Database URL (here we use a local database called medusa-development)
 const DATABASE_URL =
